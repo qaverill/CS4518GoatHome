6cdab78c2d14e58115806b1f1b97a08a15f4d1a8
Alazar Genene <alazar.genene@gmail.com>
2018-02-21 17:56:56 -0500

    Added the basic ride UI

--- app/src/main/java/edu/wpi/cs4518goathome/UserProfileEdit.java
+++ app/src/main/java/edu/wpi/cs4518goathome/UserProfileEdit.java
@@ -1,7 +1,10 @@
 package edu.wpi.cs4518goathome;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserProfileEdit extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class UserProfileEdit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile_edit);
+
+        Button requestRide = (Button) findViewById(R.id.RequestRide);
+
+        requestRide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UserProfileEdit.this, Ride.class));
+            }
+        });
     }
 }
