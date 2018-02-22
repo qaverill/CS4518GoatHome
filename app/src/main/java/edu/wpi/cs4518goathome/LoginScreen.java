c0c6dda8d768dc8d67e7435a00b531bc6eff77e8
Kevin O'Brien <kjrobrien@gmail.com>
2018-02-21 23:20:19 -0500

    Add authentication for firebase

--- app/src/main/java/edu/wpi/cs4518goathome/LoginScreen.java
+++ app/src/main/java/edu/wpi/cs4518goathome/LoginScreen.java
@@ -1,20 +1,41 @@
 package edu.wpi.cs4518goathome;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginScreen extends AppCompatActivity {
+
+    private static final String KEY = "LoginScreen";
+
+    private FirebaseAuth mAuth;
+
+    private EditText mLoginUsername;
+    private EditText mLoginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-        Button loginButton = (Button)findViewById(R.id.loginButton);
+        mAuth = FirebaseAuth.getInstance();
 
+        Button loginButton = findViewById(R.id.loginButton);
+        Button createButton = findViewById(R.id.createButton);
+        final EditText mLoginUsername = findViewById(R.id.loginUsername);
+        final EditText mLoginPassword = findViewById(R.id.loginPassword);
 
         /**
          * After logging in, if the user does not have all their information in the system, they should
@@ -24,9 +45,76 @@ public class LoginScreen extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginScreen.this, UserProfileEdit.class));
+                signIn(mLoginUsername.getText().toString(),
+                        mLoginPassword.getText().toString());
             }
         });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createUser(mLoginUsername.getText().toString(),
+                        mLoginPassword.getText().toString());
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            redirectUser();
+        }
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(KEY, "SignIn()" + email);
+        if (!validateForm()) return;
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Log.d(KEY, "Login successful!");
+                    redirectUser();
+                } else {
+                    Log.d(KEY, "Login failed!", task.getException());
+                    Toast.makeText(LoginScreen.this, "Authentication Failed!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void createUser(String email, String password) {
+        Log.d(KEY, "createUser()" + email);
+        if (!validateForm()) return;
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Log.d(KEY, "Login successful!");
+                    redirectUser();
+                } else {
+                    Log.d(KEY, "Login failed!", task.getException());
+                    Toast.makeText(LoginScreen.this, "Authentication Failed!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void redirectUser() {
+        startActivity(new Intent(this, UserProfileEdit.class));
+    }
+
+
+    private boolean validateForm() {
+        return true;
+        // TODO: validate username and password before signing in
     }
 
 }
--- app/src/main/java/edu/wpi/cs4518goathome/UserProfileEdit.java
+++ app/src/main/java/edu/wpi/cs4518goathome/UserProfileEdit.java
@@ -1,25 +1,138 @@
 package edu.wpi.cs4518goathome;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 
 public class UserProfileEdit extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private final static String KEY = "UsersProfileEdit";
+
+    private FirebaseAuth mAuth;
+    private StorageReference mStorageRef;
+
+    private EditText mUsersName;
+    private ImageView mProfilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile_edit);
 
-        Button requestRide = (Button) findViewById(R.id.RequestRide);
+        mAuth = FirebaseAuth.getInstance();
 
-        requestRide.setOnClickListener(new View.OnClickListener() {
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        mUsersName = findViewById(R.id.usersName);
+        mProfilePic = findViewById(R.id.profilePicture);
+
+        Button mSaveButton = findViewById(R.id.Save);
+
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                        .setDisplayName(mUsersName.getText().toString()).build();
+                user.updateProfile(profileUpdates);
                 startActivity(new Intent(UserProfileEdit.this, Ride.class));
             }
         });
+
+        ImageButton mEditProfilePicture = findViewById(R.id.editProfilePicture);
+
+        mEditProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            startActivity(new Intent(this, LoginScreen.class));
+        } else {
+            updateUI(user);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // set the ImageView
+            mProfilePic.setImageBitmap(imageBitmap);
+
+            // Upload the image to firebase
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            StorageReference picRef = mStorageRef.child(mAuth.getCurrentUser().getUid() + "/profilePic.png");
+            picRef.putBytes(byteArray);
+
+            // update the location of the image in the user's info
+            updateFirebaseProfilePicture(picRef.getPath());
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        mUsersName.setText(user.getDisplayName());
+
+        // load the profile picture
+        StorageReference picRef = mStorageRef.child(user.getPhotoUrl().toString());
+        try {
+            final File file = File.createTempFile("images", "png");
+            picRef.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                    mProfilePic.setImageBitmap(bitmap);
+                }
+            });
+        } catch (IOException e) {}
+    }
+
+    /**
+     * Update the user's Authentication Profile Picture
+     * @param picture A firebase cloud storage path
+     */
+    private void updateFirebaseProfilePicture(String picture) {
+        FirebaseUser user = mAuth.getCurrentUser();
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setPhotoUri(Uri.parse(picture)).build();
+        user.updateProfile(profileUpdates);
     }
 }
