6e36c13eedef4e8293fc1d07965c029821d63432
Kevin O'Brien <kjrobrien@gmail.com>
2018-02-24 14:12:11 -0500

    Can now view a trip from the map activity

--- app/src/main/java/edu/wpi/cs4518goathome/MapsActivity.java
+++ app/src/main/java/edu/wpi/cs4518goathome/MapsActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,19 +17,43 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+import edu.wpi.cs4518goathome.models.Trip;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            // return to login activity if we are not logged in
+            startActivity(new Intent(this, LoginScreen.class));
+        } else {
+        }
     }
 
 
@@ -60,18 +85,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 startActivity(viewIntent);
             }
         });
+        DatabaseReference dbRef = mDatabase.getReference("/trips");
+        ValueEventListener allTrips = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot d : dataSnapshot.getChildren()) {
+                    Trip trip = d.getValue(Trip.class);
+                    trip.setUid(d.getKey());
+                    MarkerOptions marker = new MarkerOptions().position(new LatLng(trip.latitude, trip.longitude));
+                    mMap.addMarker(marker).setTag(trip);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        dbRef.addValueEventListener(allTrips);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        MarkerOptions sydneyMarker = new MarkerOptions().position(sydney).title("Marker in Sydney");
-        mMap.addMarker(sydneyMarker).setTag("UID");
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        if(marker.getTag() != null) {
-
-        }
-        return false;
+        LatLng wpi = new LatLng(42.274637299999995, -71.806339);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(wpi, 7));
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                if(marker.getTag() != null) {
+                    Trip selectedTrip = (Trip) marker.getTag();
+                    Intent i = new Intent(MapsActivity.this, trip_view.class);
+                    i.putExtra(trip_view.EXTRA_TRIP_ID, selectedTrip.getUid());
+                    startActivity(i);
+                    Log.i("MapsActivity", selectedTrip.name);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/edu/wpi/cs4518goathome/trip_view.java
+++ app/src/main/java/edu/wpi/cs4518goathome/trip_view.java
@@ -1,10 +1,13 @@
 package edu.wpi.cs4518goathome;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 import android.os.Bundle;
@@ -16,18 +19,106 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.net.Uri;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+
+import edu.wpi.cs4518goathome.models.Trip;
+import edu.wpi.cs4518goathome.models.User;
+
 
 public class trip_view extends AppCompatActivity {
 
+    private ImageView mProfilePic;
+    private TextView mDriverName;
+    private TextView mDriverMajor;
+    private TextView mDriverPhone;
+    private Button mCallDriver;
+    private TextView mTripDestination;
+    private TextView mTripDescription;
 
+    private FirebaseDatabase mDatabase;
+    private FirebaseStorage mStorage;
+
+    public static final String EXTRA_TRIP_ID = "TripId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_view);
+        mProfilePic = findViewById(R.id.profilePicture);
+        mDriverName = findViewById(R.id.driverName);
+        mDriverMajor = findViewById(R.id.driverMajor);
+        mDriverPhone = findViewById(R.id.driverPhoneNumber);
+        mCallDriver = findViewById(R.id.callDriver);
+        mTripDestination = findViewById(R.id.tripDestination);
+        mTripDescription = findViewById(R.id.tripDescription);
+
+        String tripId = getIntent().getStringExtra(EXTRA_TRIP_ID);
+        if (tripId != null) {
+        } else {
+            //TODO: do something if extra isn't provided
+        }
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mStorage = FirebaseStorage.getInstance();
+        final StorageReference mStorageRef = mStorage.getReference();
+
+        final ValueEventListener driverListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User driver = dataSnapshot.getValue(User.class);
+                mDriverName.setText(driver.name);
+                mDriverMajor.setText(driver.major);
+                mDriverPhone.setText(driver.phoneNumber);
+                StorageReference picRef = mStorageRef.child(User.getProfilePic(dataSnapshot.getKey()));
+                try {
+                    final File file = File.createTempFile("images", "png");
+                    picRef.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                            mProfilePic.setImageBitmap(bitmap);
+                        }
+                    });
+                } catch (IOException e) {
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        DatabaseReference tripRef = mDatabase.getReference("/trips").child(tripId);
+        ValueEventListener tripListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Trip selectedTrip = dataSnapshot.getValue(Trip.class);
+                mTripDestination.setText(selectedTrip.name);
+                mTripDescription.setText(selectedTrip.desc);
+                DatabaseReference driverRef = mDatabase.getReference("/users").child(selectedTrip.driver);
+                driverRef.addListenerForSingleValueEvent(driverListener);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        tripRef.addListenerForSingleValueEvent(tripListener);
+
     }
 
-    Button callDriverButton = (Button) findViewById(R.id.callDriver) ;
 
 
     public void callDriver (Button callDriver) {
